dc222fc8222b784894c57664398b0ece8ade971d
miland <miland@uni-koblenz.de>
2015-10-02 16:06:51 +0200

    Commit-Nachricht

--- framework/CG_AnSim_demos/src/executables/assignment/main.cpp
+++ framework/CG_AnSim_demos/src/executables/assignment/main.cpp
@@ -34,6 +34,69 @@ std::vector<glm::vec3> *mVertices;
 std::vector<glm::vec3> *mTangents;
 int count = 0;
 
+//*************************************************************************************************************
+float timeRunning 	= 0.0f;
+float position 		= 0.0f;
+float speed 		= 1.0f;
+glm::vec3 renderPosition 	= glm::vec3(0.0f,0.0f,0.0f);
+glm::vec3 tangent 		= glm::vec3(1.0f,0.0f,0.0f);
+glm::vec3 normal		= glm::vec3(0.0f,1.0f,0.0f);
+glm::vec3 binormal		= glm::vec3(0.0f,0.0f,1.0f);
+
+float teapot_mass 			= 20.0f;
+float teapot_acceleration 	= 0.0f;
+float gravity 				= 9.81f;
+float F_g 					= teapot_mass * gravity;
+float F 					= 0.0f;
+glm::vec3 down				= glm::vec3(0.0f,-1.0f,0.0f);
+glm::vec3 curTangent;
+//*************************************************************************************************************
+
+
+void updateTeapot(float d_t)
+{
+		timeRunning += d_t;
+
+		float dot = down.x * curTangent.x + down.y * curTangent.y + down.z * curTangent.z;
+		float cosA = dot / (down.length() * curTangent.length());
+
+		F = F_g * -cosA;
+		teapot_acceleration = F / teapot_mass;
+		speed = teapot_acceleration * d_t + speed;
+
+		position = timeRunning * speed;
+
+
+	float u;
+	int patchNum;
+	mPath->getParameterByArcLength(position, u, patchNum);
+	if(u < 0.0f){ //wenn der Teapot am Ende des Pfades angekommen ist wird er an die Ausgangsposition zurückgesetzt
+		position = 0.0f;
+		timeRunning = 0.0f;
+		return;
+	}
+
+	//jetzt  haben wir Parameter u und den index des entsprechenden Splines, nun können wir diesen evaluieren
+	//und erhalten die Position des Teekessels.
+	glm::vec3 t;
+	mPath->evaluateHermiteSpline(u,
+								  (*(mPath->getControlPointsPtr()))[patchNum],
+								  (*(mPath->getControlPointsPtr()))[patchNum + 1],
+								  renderPosition,
+								  t);
+
+
+
+	//Orientierung des Teapots mittels der Frenet Frame Methode
+	mPath->calculateFrenetFrame(u,
+								 (*(mPath->getControlPointsPtr()))[patchNum],
+								 (*(mPath->getControlPointsPtr()))[patchNum + 1],
+								 tangent,
+								 binormal,
+								 normal);
+	curTangent = t;
+
+}
 
 void resizeCallback( GLFWwindow *window, int w, int h)
 {
@@ -58,41 +121,47 @@ void init_materials()
 
 void init_scene()
 {
-	scene_node = new CVK::Node( "Scene");
+	scene_node = new CVK::Node( "Scene" );
 
 	mPath = new CVK::HermiteSpline();
+	mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(0.0, 6.0, 5.0), glm::vec3(-5.0, 1.0, 0.0)));
 	mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(-5.0, 1.0, 0.0), glm::vec3(0.0, 4.0, -5.0)));
 	mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(0.0, 4.0, -5.0), glm::vec3(5.0, 1.0, 0.0)));
 	mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(5.0, 1.0, 0.0), glm::vec3(0.0, 6.0, 5.0)));
 	mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(0.0, 6.0, 5.0), glm::vec3(-5.0, 1.0, 0.0)));
-	mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(-5.0, 1.0, 0.0), glm::vec3(0.0, 4.0, -5.0)));
-	mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(0.0, 4.0, -5.0), glm::vec3(5.0, 1.0, 0.0)));
+	//mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(-5.0, 1.0, 0.0), glm::vec3(0.0, 4.0, -5.0)));
+
+	//mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(-5.0, 1.0, 0.0), glm::vec3(0.0, 4.0, -5.0)));
+	//mPath->addControlPoint(new CVK::HermiteSplineControlPoint(glm::vec3(0.0, 4.0, -5.0), glm::vec3(5.0, 1.0, 0.0)));
 	mVertices = mPath->getVerticesPtr();
 	mTangents = mPath->getTangentsPtr();
-
 	mPath->generateRenderVertices();
+
+	renderPosition = (mPath->getControlPointsPtr()->at(0))->getPosition();
+	mPath->generateArcLengthTable(100);
 
 	//for a hermitSplineRail
 	rail = new CVK::Rail();
- 	for (int i = 0; i < mPath->getVerticesPtr()->size(); i++) {
+	for (int i = 0; i < mPath->getVerticesPtr()->size(); i++) {
 		rail->addPoint(mPath->getVerticesPtr()->at(i));
 		rail->addTangent(mPath->getTangentsPtr()->at(i), mPath->getVerticesPtr()->size());
 	}
+
+	curTangent = mPath->getTangentsPtr()->at(0);
 
 	CVK::Node *rail_node_up = new CVK::Node("Rail_up");
 	rail_node_up->setModelMatrix( glm::translate(glm::mat4( 1.0f), glm::vec3( 0, 0, 0)));
 	rail_node_up->setMaterial( mat_red);
 	rail_node_up->setGeometry( rail);
 	scene_node->addChild( rail_node_up);
-
 }
 
-int main() 
+int main()
 {
 	// Init GLFW and GLEW
 	glfwInit();
 	CVK::useOpenGL33CoreProfile();
-	window = glfwCreateWindow(WIDTH, HEIGHT, "AnSim Exercise 1", 0, 0);
+	window = glfwCreateWindow(WIDTH, HEIGHT, "AnSim Achterbahn", 0, 0);
 	glfwSetWindowPos( window, 100, 50);
 	glfwMakeContextCurrent(window);
 	glfwSetWindowSizeCallback( window, resizeCallback);
@@ -102,8 +171,8 @@ int main()
 	glm::vec3 BgCol = CVK::State::getInstance()->getBackgroundColor();
 	glClearColor( BgCol.r, BgCol.g, BgCol.b, 0.0);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);         
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	//load, compile and link Shader
 	const char *shadernames[2] = { SHADERS_PATH "/Phong.vert", SHADERS_PATH "/Phong.frag" };
@@ -112,6 +181,13 @@ int main()
 	const char *shadernames2[2] = { SHADERS_PATH "/ColorMVPShader.vert", SHADERS_PATH "/ColorMVPShader.frag" };
 	CVK::ShaderLineRender lineShader(VERTEX_SHADER_BIT | FRAGMENT_SHADER_BIT, shadernames2);
 	CVK::State::getInstance()->setShader(&lineShader);
+
+	//Teapot
+	CVK::Node teapot("Teapot");
+	CVK::Material material = CVK::Material(red, white, 100.0f);
+	teapot.setMaterial(&material);
+	CVK::Teapot geometry = CVK::Teapot();
+	teapot.setGeometry(&geometry);
 
 	init_camera();
 	init_materials();
@@ -123,16 +199,22 @@ int main()
 	CVK::State::getInstance()->updateSceneSettings(darkgrey, 0, white, 1, 10, 1);
 
 	glLineWidth(6);
+	float deltaTime = 0.0f;
+	float oldTime = static_cast<float>(glfwGetTime());
 
 	CVK::CoordinateSystem coords = CVK::CoordinateSystem(glm::vec3(0.0f));
-	
+
 	while( !glfwWindowShouldClose( window))
 	{
+		float currentTime = static_cast<float>(glfwGetTime());
+		deltaTime = currentTime - oldTime;
+		oldTime = currentTime;
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
+
 		//Update Camera and following the spline by pressing space
 		if (glfwGetKey( window, GLFW_KEY_SPACE) == GLFW_PRESS)   {
-			if(count == mPath->getVerticesPtr()->size()-100){
+			if(count == mPath->getVerticesPtr()->size()){
 				count = 0;
 			}
 			cam_trackball.setCenter(&mVertices->at(count));
@@ -140,6 +222,8 @@ int main()
 			count++;
 		}
 		cam_trackball.update( window);
+
+		updateTeapot(deltaTime);
 
 		//Use Shader and define camera uniforms
 		CVK::State::getInstance()->setShader(&phongShader);
@@ -149,6 +233,18 @@ int main()
 		CVK::State::getInstance()->setLight( 0, plight);
 
 		scene_node->render();
+
+		glm::mat4 modelmatrix = glm::transpose(glm::mat4(
+				tangent.x, 	tangent.y, 	tangent.z, 	renderPosition.x,
+				binormal.x, binormal.y, binormal.z, renderPosition.y,
+				normal.x, 	normal.y, 	normal.z, 	renderPosition.z,
+				0.0f,		0.0f,		0.0f,		1.0f));
+
+
+
+		modelmatrix = glm::scale(modelmatrix, glm::vec3(0.5f,0.5f,0.5f));
+		teapot.setModelMatrix(modelmatrix);
+		teapot.render();
 
 		glClear(GL_DEPTH_BUFFER_BIT);
 		CVK::State::getInstance()->setShader(&lineShader);
--- framework/libraries/CVK_AnSim/CVK_AS_HermiteSpline.cpp
+++ framework/libraries/CVK_AnSim/CVK_AS_HermiteSpline.cpp
@@ -1,6 +1,7 @@
 #include "CVK_AS_HermiteSpline.h"
 
 #define ONE_THIRD (1.0f / 3.0f)
+#define THRESH 0.001f
 
 CVK::HermiteSpline::HermiteSpline()
 {
@@ -16,6 +17,148 @@ void CVK::HermiteSpline::generateRenderVertices(){
 		sampleHermiteSpline(*cpIt, *(cpIt + 1), 100);
 		cpIt++;
 	}
+}
+
+void CVK::HermiteSpline::generateArcLengthTable(int resolution)
+{
+
+	int numPatches = mControlPoints.size() - 1;
+	int stepsPerPatch = resolution / numPatches;
+	float u;
+	float uStep = 1.0f / stepsPerPatch;
+	float len = 0.0;
+
+	glm::vec3 point, lastPoint;
+
+	for(int i=0; i<numPatches; i++){
+
+		u = 0.0f;
+		for(int j=0; j<=stepsPerPatch; j++){
+
+			if(i != 0 && j == 0){
+				u += uStep;
+				continue;
+			}
+
+			glm::vec3 t;
+			evaluateHermiteSpline(u, mControlPoints[i], mControlPoints[i + 1], point, t);
+
+			ArcLengthTableEntry ale;
+			ale.u = u;
+			ale.patchNumber = i;
+			if(i == 0 && j == 0){
+				ale.arcLength = 0.0f;
+			}
+			else{
+				ale.arcLength = (point - lastPoint).length() + mArcLengthTable.back().arcLength;
+			}
+
+			lastPoint = point;
+			mArcLengthTable.push_back(ale);
+
+			u += uStep;
+
+		}
+	}
+
+}
+
+void CVK::HermiteSpline::getParameterByArcLength(float length, float &u, int &patchNum)
+{
+
+	if(length > mArcLengthTable.back().arcLength){ //given length exeeds path length, return -1 as error
+		u = -1.0;
+		patchNum = -1;
+		return;
+	}
+
+	bool found = false;
+	int index;
+	int indexLow = 0;
+	int indexHi = mArcLengthTable.size() - 1;
+
+	while(!found){
+		index = indexLow + 0.5 * (indexHi - indexLow);
+
+		if(mArcLengthTable[index].arcLength == length){ //found
+			u = mArcLengthTable[index].u;
+			patchNum = mArcLengthTable[index].patchNumber;
+			found = true;
+		}
+
+		if(mArcLengthTable[index].arcLength > length){
+			if(index - 1 >= 0){
+				if(mArcLengthTable[index-1].arcLength < length){
+
+					float frac = (length - mArcLengthTable[index-1].arcLength) / (mArcLengthTable[index].arcLength - mArcLengthTable[index-1].arcLength);
+
+					float interpolLow = mArcLengthTable[index - 1].u;
+					/*
+					Ueberpr¸fung: der kleinere Wert ist der letzte Wert des vorherigen Patch
+					In der Bogenl‰ngentabelle vgl.  Vorlesungsskript fehlt der Wert f¸r u = 0
+					Daher Wert f¸r u=1 des vorherigen Patches nehmen.
+					*/
+
+					//if(interpolLow >= 1.0f){
+					if(interpolLow >= 1.0f - THRESH && interpolLow <= 1.0f + THRESH){
+						interpolLow = 0.0f;
+					}
+					float interpolHi =  mArcLengthTable[index].u;
+					u = interpolLow + frac * (interpolHi - interpolLow);
+					patchNum = mArcLengthTable[index].patchNumber;
+
+					found = true;
+				}
+				else{
+					indexHi = index - 1;
+				}
+			}
+		}
+
+		if(mArcLengthTable[index].arcLength < length){
+			if(index + 1 < mArcLengthTable.size()){ //bounds check
+				if(mArcLengthTable[index + 1].arcLength > length){
+
+					float frac = (length - mArcLengthTable[index].arcLength) / (mArcLengthTable[index + 1].arcLength - mArcLengthTable[index].arcLength);
+
+					float interpolLow = mArcLengthTable[index].u;
+					//if(interpolLow >= 1.0f){
+					if(interpolLow >= 1.0f - THRESH && interpolLow <= 1.0f + THRESH){
+						interpolLow = 0.0f;
+					}
+					float interpolHi =  mArcLengthTable[index + 1].u;
+					u = interpolLow + frac * (interpolHi - interpolLow);
+					patchNum = mArcLengthTable[index + 1].patchNumber;
+
+					found = true;
+				}
+				else{
+					indexLow = index + 1;
+				}
+			}
+		}
+	}
+}
+
+void CVK::HermiteSpline::calculateFrenetFrame(float u, HermiteSplineControlPoint* c0, HermiteSplineControlPoint* c1, glm::vec3 &tangent, glm::vec3 &binormal, glm::vec3 &normal)
+{
+	std::vector<glm::vec3> bez;
+	bez.push_back(c0->getPosition());
+	bez.push_back(c0->getPosition() + ONE_THIRD * c0->getDirection());
+	bez.push_back(c1->getPosition() - ONE_THIRD * c1->getDirection());
+	bez.push_back(c1->getPosition());
+
+	glm::vec3 point, t;
+	deCasteljau(u, bez, point, tangent);
+
+	glm::vec3 secDer =  (6.0f - 6.0f * u) * bez[0] +
+						(18.0f * u - 12.0f) * bez[1] +
+						(6.0f - 18.0f * u) * bez[2] +
+						6.0f * u * bez[3];
+
+	binormal = glm::normalize(glm::cross(tangent, secDer));
+	normal = glm::normalize(glm::cross(binormal, tangent));
+
 }
 
 void CVK::HermiteSpline::sampleHermiteSpline(HermiteSplineControlPoint* c0, HermiteSplineControlPoint* c1, int numVerts)
@@ -53,13 +196,13 @@ void CVK::HermiteSpline::deCasteljau(float t, std::vector<glm::vec3> &cp, glm::v
 	}
 	else
 	{
-	std::vector<glm::vec3> ncp;
-	std::vector<glm::vec3>::iterator it = cp.begin();
-	while( it != cp.end()-1)
-	{
-		ncp.push_back((1-t) * (*it) + t * (*(it+1)));
-		it ++;
-	}
-	deCasteljau(t, ncp, point, tangent);
+		std::vector<glm::vec3> ncp;
+		std::vector<glm::vec3>::iterator it = cp.begin();
+		while( it != cp.end()-1)
+		{
+			ncp.push_back((1-t) * (*it) + t * (*(it+1)));
+			it ++;
+		}
+		deCasteljau(t, ncp, point, tangent);
 	}
 }
